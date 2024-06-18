e0dc6cb5afb0a0e4235f9e1ccb7fb39632941201
Vitaliy-datascientist <vitaliy.dayneko2410@gmail.com>
2024-06-18 23:59:34 +0300

    second task(updated 2)

--- second_task.py
+++ second_task.py
@@ -3,18 +3,16 @@
 import random
 
 
-def get_numbers_ticket(min, max, quantity):
+def get_numbers_ticket(min_num, max_num, quantity):
     """Параметри функції набувають таких обмежень:
-       1. min - не менше 1;
-       2. max - не більше 1000.
+       1. min_num - не менше 1;
+       2. max_num - не більше 1000.
        Поверне відсортований список випадкових цифр
                                 від min до max включно, розміром quantity"""
-    try:
-        if min < 1 or max > 1000:
-            raise ValueError
-        else:
-            population = range(min, max + 1)
-            res = random.sample(population, k=quantity)
-            return sorted(res)
-    except ValueError:
+    population = range(min_num, max_num + 1)
+    if any((min_num < 1, max_num > 1000, min_num > max_num,
+            quantity > len(population))):
         return []
+    else:
+        res = random.sample(population, k=quantity)
+        return sorted(res)
