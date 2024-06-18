516fb1c71e76548b3f6a2664a1e3247efe4c7b7f
Vitaliy-datascientist <vitaliy.dayneko2410@gmail.com>
2024-06-18 18:07:07 +0300

    first task(updated)

--- first_task.py
+++ first_task.py
@@ -6,9 +6,9 @@ from datetime import datetime
 def get_days_from_today(date):
     """Функція розраховує кількість днів між заданою датою і поточною датою"""
     try:
-        obj_datetime = datetime.strptime(date, '%Y-%m-%d')
-        today = datetime.today()
-        difference = obj_datetime - today
+        date = datetime.strptime(date, '%Y-%m-%d').date()
+        today_date = datetime.today().date()
+        difference = today_date - date
         return difference.days
     except ValueError:
         return 'Неправильний формат вхідних даних'
