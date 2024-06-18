80d9830448daafad9c25f8bd86039d097e7d853f
Vitaliy-datascientist <vitaliy.dayneko2410@gmail.com>
2024-06-18 18:40:21 +0300

    second task(updated)

--- second_task.py
+++ second_task.py
@@ -9,9 +9,12 @@ def get_numbers_ticket(min, max, quantity):
        2. max - не більше 1000.
        Поверне відсортований список випадкових цифр
                                 від min до max включно, розміром quantity"""
-    if min < 1 or max > 1000:
-        return 'Вхідні параметри не відповідають заданим обмеженням'
-    else:
-        population = range(min, max + 1)
-        res = random.sample(population, k=quantity)
-        return sorted(res)
+    try:
+        if min < 1 or max > 1000:
+            raise ValueError
+        else:
+            population = range(min, max + 1)
+            res = random.sample(population, k=quantity)
+            return sorted(res)
+    except ValueError:
+        return []
